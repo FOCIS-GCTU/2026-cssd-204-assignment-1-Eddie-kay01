338aebf8a045d3dd9039a344929c33e35e8f72bb
Eddie-kay01 <edwindarko704@gmail.com>
2026-05-31 11:17:07 +0000

    Add files via upload

---
+++ E.K.D.py
@@ -0,0 +1,31 @@
+
+#File name: E.K.D.py
+#Description: This code prints My  initials in a stylsh ASCII way using only the letter E,K and D
+#Assignment Number: 1 
+#Name: Edwin Darko
+#Student ID:2425401989
+#Email:2425401989@live.gctu.edu.gh
+#Grader Name: Mr. Augustus Buckman 
+#On my honour,Edwin Darko, this programming assignment is my own work and I have not provided this code to any other student.
+
+def main():
+	# This code shows my initials in a stylish way using only the letters E, K and D
+	print()
+	print("...EKD")
+	print()
+	print("...EEEEEEEEEEEE........KK........KK........DDDDDDDD.........")
+	print("...EEEEEEEEEEEE........KK.......KK.........DDDDDDDDD........")
+	print("...EE..................KK.....KK...........DD......DD.......")
+	print("...EE..................KK...KK.............DD.......DD......")
+	print("...EEEEEEEEEEEE........KK..KK..............DD........DD.....")
+	print("...EEEEEEEEEEEE........KK...KK.............DD........DD.....")
+	print("...EE..................KK....KK............DD........DD.....")
+	print("...EE..................KK.....KK...........DD.......DD......")
+	print("...EE..................KK......KK..........DD......DD.......")
+	print("...EEEEEEEEEEEE...**...KK.......KK....**...DDDDDDDDD......**")
+	print("...EEEEEEEEEEEE...**...KK........KK...**...DDDDDDD........**")
+	print()
+
+
+if __name__ == "__main__":
+	main()
